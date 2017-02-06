8c144c218bce8a54d218c70c78618a5549238cb8
Trion3000 <vasil.sokoliuk@gmail.com>
2017-02-06 12:46:01 +0200

    Attempt #1

---
+++ 23.php
@@ -0,0 +1,8 @@
+﻿<?php
+// 23. Напишите 3 строки, используя три разные функции для вывода текста.
+
+echo 'blah-blah-blah </br>';
+print ("blah-blah-blah </br>");
+printf ("blah-blah-blah </br>"); 
+
+?>
